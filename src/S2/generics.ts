4caa1ee12a2c3bca19e27c416a3648696d2260f9
dev02tech2w <132919758+dev02tech2w@users.noreply.github.com>
2025-02-27 10:10:14 +0100

    S2 finie

--- src/S2/generics.ts
+++ src/S2/generics.ts
@@ -15,19 +15,38 @@
 
 // }
 
-interface Lengthwise {
-  length: number;
+// interface Lengthwise {
+//   length: number;
+// }
+
+// const gh: Array<any> = [];
+// const ghdf: Array<any> = ["12", "4596"];
+
+// const premierElement = <Type extends Lengthwise>(a: Type): Array<any> => {
+//   a.length > 0 ? console.log(a[0]) : console.log("Pas un vrai tableau");
+
+//   return a.length > 0 ? a[0] : "Pas un vrai tableau";
+// };
+
+// premierElement(gh);
+// premierElement(ghdf);
+
+interface Voiture {
+  marque: string;
+  modele: string;
+  annee: number;
 }
 
-const gh = []
-const ghdf = ["12","4596"]
-
-const premierElement =<Type extends Lengthwise>(a: Type): any  => {
-
-  a.length > 0 ? console.log(a[0]) : console.log("Pas un vrai tableau")
-  
-  return a.length > 0 ? a[0] : "Pas un vrai tableau"
+const maVoiture: Voiture = {
+  marque: "Ford",
+  modele: "Mustang GT350",
+  annee: 1998,
 };
 
-premierElement(gh)
-premierElement(ghdf)
+const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
+  console.log(obj[key]);
+
+  return obj[key];
+};
+
+getProperty(maVoiture, "annee");
